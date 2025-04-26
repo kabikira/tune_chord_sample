0b23c702b7b3df9472d57b7e6246e8cb413b5d09
kabikira <kabikirar2@protonmail.com>
2025-04-26 20:16:29 +0900

    add ナビゲーションタブを2回連続タップしたときルートに戻る

--- lib/src/pages/navBar/nav_bar.dart
+++ lib/src/pages/navBar/nav_bar.dart
@@ -1,19 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-class ScaffoldWithNavBar extends StatelessWidget {
+class ScaffoldWithNavBar extends StatefulWidget {
   final StatefulNavigationShell navigationShell;
 
   const ScaffoldWithNavBar({super.key, required this.navigationShell});
 
   @override
+  State<ScaffoldWithNavBar> createState() => _ScaffoldWithNavBarState();
+}
+
+class _ScaffoldWithNavBarState extends State<ScaffoldWithNavBar> {
+  int? _lastTappedIndex;
+  DateTime? _lastTapTime;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: navigationShell,
+      body: widget.navigationShell,
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: navigationShell.currentIndex,
+        currentIndex: widget.navigationShell.currentIndex,
         onTap: (index) {
-          navigationShell.goBranch(index);
+          final now = DateTime.now();
+
+          // 同じタブを短時間内に2回タップした場合
+          if (index == _lastTappedIndex &&
+              _lastTapTime != null &&
+              now.difference(_lastTapTime!).inMilliseconds < 500) {
+            // 現在のブランチのルートに移動
+            switch (index) {
+              case 0:
+                context.go('/tuningList');
+                break;
+              case 1:
+                context.go('/settings');
+                break;
+            }
+            // タップ情報をリセット
+            _lastTappedIndex = null;
+            _lastTapTime = null;
+          } else {
+            // 異なるタブをタップした場合や初回タップの場合は通常通りブランチを切り替える
+            widget.navigationShell.goBranch(index);
+
+            // タップ情報を更新
+            _lastTappedIndex = index;
+            _lastTapTime = now;
+          }
         },
         items: const [
           BottomNavigationBarItem(
